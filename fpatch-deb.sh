c819939c2343f37d8e0501fa8358af7723936679
Siwat Sirichai <siwat2545@cud55.com>
2020-07-22 19:41:58 +0700

    Update fpatch-deb.sh

--- fpatch-deb.sh
+++ fpatch-deb.sh
@@ -1,4 +1,4 @@
-apt-get -y install autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libgnutls28-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget yasm zlib1g-dev libunistring-dev
+apt-get -y install autoconf frei0r-plugins automake build-essential cmake git-core libass-dev libfreetype6-dev libgnutls28-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget yasm zlib1g-dev libunistring-dev
 mkdir -p ~/ffmpeg_sources ~/bin
 apt-get -y install nasm libx264-dev libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git && cd nv-codec-headers && make && make install
